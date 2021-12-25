6c198a0b782ea3781eb64d6e7c8b78c662d1a60d
Fpicacho <2314008827@qq.com>
2021-12-25 21:50:58 +0800

    <init> 实现增加列表功能

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+// import HelloWorld from './components/HelloWorld.vue'
+import ToDoList from "./components/ToDoList.vue";
+</script>
+
+<template>
+  <ToDoList/>
+</template>
+
+<style>
+
+</style>
---
+++ src/components/ToDoList.vue
@@ -0,0 +1,40 @@
+<template>
+  <!--待办输入框-->
+  <input type="text" placeholder="请输入代办事项" v-model="newToDo" @keyup.enter="addToDo"/>
+  <!--代办列表-->
+  <ul>
+    <li v-for="item in ToDoList" :key="item.id">{{item.content}}</li>
+  </ul>
+</template>
+
+<script>
+import {reactive, toRefs} from 'vue'
+
+export default {
+  name: "ToDoList",
+  setup() {
+    const state = reactive({
+      newToDo: "",
+      ToDoList: []
+    })
+    function addToDo(){
+      state.ToDoList.push(
+          {
+            id:state.ToDoList.length + 1,
+            content:state.newToDo,
+            isDone:false
+          }
+      )
+      state.newToDo = ''
+    }
+    return {
+      ...toRefs(state),
+      addToDo
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
